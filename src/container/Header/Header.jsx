be8638402be8519e9b5bbb6d077a766974afb1b4
haafeezdr <haafeezdaiyab>
2024-02-23 16:43:19 +0100

    header,navbar,about

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import { images } from '../../constants';
 import './Header.css';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+       <SubHeading title="Chase the new flavour"/> 
+       <h1 className='app__header-h1'>The Key To Fine Dining</h1>
+       <p className='p__opensans' style={{ margin: '2rem 0'}}>here we searve the very best of continental dishes and also local quesine, THE VERY BEST IN TOWN.</p>
+       <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+    <div className='app__wrapper_img'>
+        <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { SubHeading, MenuItem } from '../../components';
+import { images, data } from '../../constants';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialMenu flex__center section__padding' id='menu'>
+    <div className='app__specialMenu-title'>
+      <SubHeading title='Menu That Fit Your Palatte'/>
+    </div>
   </div>
 );
 
